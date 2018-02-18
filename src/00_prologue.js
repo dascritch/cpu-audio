49447abba4e83dafa05a0d415fd1d44f122c248c
dascritch <xaviermd@gmail.com>
2018-02-18 21:41:22 +0200

    Remove vestigial class="" for id=""

--- src/00_prologue.js
+++ src/00_prologue.js
@@ -3,6 +3,6 @@ let template, shadow_element;
 const thisDoc = (document._currentScript || document.currentScript).ownerDocument;
 const CpuAudioTagName = 'CPU-AUDIO';
 const CpuControllerTagName = 'CPU-CONTROLLER';
-const selector_interface = '.interface';
+const selector_interface = '#interface';
 const acceptable_selector = 'audio[controls]';
 const acceptable_hide_atttributes = ['actions', 'chapters'];
