41238161a6a9e8fe90aa8acf834084436eee03f6
dascritch <xaviermd@gmail.com>
2018-08-12 15:23:32 +0200

    Porting QUint from 1.14 (!) to 2.6

--- tests/tests-browser.js
+++ tests/tests-browser.js
@@ -1,13 +1,13 @@
 
 var audiotag = document.getElementById('track');
 
-test( "Checking browser abilities for WebComponents", function() {
-	ok( window.customElements !== undefined, "window.customElements" );
+QUnit.test( "Checking browser abilities for WebComponents", function( assert ) {
+	assert.ok( window.customElements !== undefined, "window.customElements" );
 });
 
-test( "<audio> tag API, fallback suite for seekElementAt()", function() {
-	ok( audiotag.fastSeek !== undefined, "seekElementAt can use audiotag.fastSeek" );
-	ok( audiotag.currentTime !== undefined, "seekElementAt can use audiotag.currentTime" );
+QUnit.test( "<audio> tag API, fallback suite for seekElementAt()", function( assert ) {
+	assert.ok( audiotag.fastSeek !== undefined, "seekElementAt can use audiotag.fastSeek" );
+	assert.ok( audiotag.currentTime !== undefined, "seekElementAt can use audiotag.currentTime" );
 });
 
 
--- tests/tests-minimal.js
+++ tests/tests-minimal.js
@@ -1,10 +1,13 @@
+QUnit.config.autostart = false;
+
 window.addEventListener('WebComponentsReady', function() {
 
 	window.location = '#';
-	var lock = false;
-	var lockedAt = 0;
-	var audiotag = document.getElementById('track');
-	var playground = document.getElementById('playground');
+	let lock = false;
+	let lockedAt = 0;
+	let audiotag = document.getElementById('track');
+	let playground = document.getElementById('playground');
+	audiotag.volume = 0;
 
 	function stopPlayer() {
 		audiotag.pause();
@@ -23,102 +26,104 @@ window.addEventListener('WebComponentsReady', function() {
 	}
 
 	QUnit.testDone(stopPlayer);
+	QUnit.start();
 
-	test( "hello CPU API", function() {
-		ok( typeof document.CPU === "object", "Passed!" );
-		ok(audiotag.paused, 'paused by defaults' );
+	QUnit.test( "hello CPU API", function( assert ) {
+		assert.ok( typeof document.CPU === "object", "Passed!" );
+		assert.ok(audiotag.paused, 'paused by defaults' );
 	});
 
 	let convert = document.CPU.convert;
 
 	waitNoLock();
 	nowLock();
-	test( "document.CPU.convert.TimeInSeconds", function() {
-		ok(convert.TimeInSeconds(0) === 0, 'got zero' );
-		ok(convert.TimeInSeconds('1') === 1, 'got one' );
-		ok(convert.TimeInSeconds('1s') === 1, 'got one second' );
-		ok(convert.TimeInSeconds('20s') === 20, 'got twenty seconds' );
-		ok(convert.TimeInSeconds('1m') === 60, 'got one minute' );
-		ok(convert.TimeInSeconds('1m1s') === 61, 'got one minute and one second' );
-		ok(convert.TimeInSeconds('2h4m2s') === 7442, 'got 2 hours, 4 minutes and 2 seconds' );
+	QUnit.test( "document.CPU.convert.TimeInSeconds", function( assert ) {
+		assert.ok(convert.TimeInSeconds(0) === 0, 'got zero' );
+		assert.ok(convert.TimeInSeconds('1') === 1, 'got one' );
+		assert.ok(convert.TimeInSeconds('1s') === 1, 'got one second' );
+		assert.ok(convert.TimeInSeconds('20s') === 20, 'got twenty seconds' );
+		assert.ok(convert.TimeInSeconds('1m') === 60, 'got one minute' );
+		assert.ok(convert.TimeInSeconds('1m1s') === 61, 'got one minute and one second' );
+		assert.ok(convert.TimeInSeconds('2h4m2s') === 7442, 'got 2 hours, 4 minutes and 2 seconds' );
 	});
 
-	test( "document.CPU.convert.ColonTimeInSeconds", function() {
-		ok(convert.ColonTimeInSeconds('0:01') === 1, 'got one second' );
-		ok(convert.ColonTimeInSeconds('1:34') === 94, 'got one minute and 34 seconds' );
-		ok(convert.ColonTimeInSeconds('2:01:34') === 7294, 'got two hours, one minute and 34 seconds' );
-		ok(convert.ColonTimeInSeconds('1:02:01:34') === (7294 + 86400), 'got one day, two hours, one minute and 34 seconds' );
+	QUnit.test( "document.CPU.convert.ColonTimeInSeconds", function( assert ) {
+		assert.ok(convert.ColonTimeInSeconds('0:01') === 1, 'got one second' );
+		assert.ok(convert.ColonTimeInSeconds('1:34') === 94, 'got one minute and 34 seconds' );
+		assert.ok(convert.ColonTimeInSeconds('2:01:34') === 7294, 'got two hours, one minute and 34 seconds' );
+		assert.ok(convert.ColonTimeInSeconds('1:02:01:34') === (7294 + 86400), 'got one day, two hours, one minute and 34 seconds' );
 	});
 
-	test( "document.CPU.convert.SecondsInTime", function() {
-		ok(convert.SecondsInTime(0) === '0s', 'got zero' );
-		ok(convert.SecondsInTime(1) === '1s', 'got one' );
-		ok(convert.SecondsInTime(20) === '20s', 'got twenty seconds' );
-		ok(convert.SecondsInTime(60) === '1m0s', 'got one minute' );
-		ok(convert.SecondsInTime(61) === '1m1s', 'got one minute and one second' );
-		ok(convert.SecondsInTime(7442) === '2h4m2s', 'got 2 hours, 4 minutes and 2 seconds' );
+	QUnit.test( "document.CPU.convert.SecondsInTime", function( assert ) {
+		assert.ok(convert.SecondsInTime(0) === '0s', 'got zero' );
+		assert.ok(convert.SecondsInTime(1) === '1s', 'got one' );
+		assert.ok(convert.SecondsInTime(20) === '20s', 'got twenty seconds' );
+		assert.ok(convert.SecondsInTime(60) === '1m0s', 'got one minute' );
+		assert.ok(convert.SecondsInTime(61) === '1m1s', 'got one minute and one second' );
+		assert.ok(convert.SecondsInTime(7442) === '2h4m2s', 'got 2 hours, 4 minutes and 2 seconds' );
 	});
 
-	test( "document.CPU.convert.SecondsInColonTime", function() {
-		ok(convert.SecondsInColonTime(0) === '0:00', 'got 0:00' );
-		ok(convert.SecondsInColonTime(1) === '0:01', 'got 0:01' );
-		ok(convert.SecondsInColonTime(20) === '0:20', 'got 0:20' );
-		ok(convert.SecondsInColonTime(60) === '1:00', 'got 1:00' );
-		ok(convert.SecondsInColonTime(61) === '1:01', 'got 1:01' );
-		ok(convert.SecondsInColonTime(130) === '2:10', 'got 2:10' );
-		ok(convert.SecondsInColonTime(7442) === '2:04:02', 'got 2:04:02' );
+	QUnit.test( "document.CPU.convert.SecondsInColonTime", function( assert ) {
+		assert.ok(convert.SecondsInColonTime(0) === '0:00', 'got 0:00' );
+		assert.ok(convert.SecondsInColonTime(1) === '0:01', 'got 0:01' );
+		assert.ok(convert.SecondsInColonTime(20) === '0:20', 'got 0:20' );
+		assert.ok(convert.SecondsInColonTime(60) === '1:00', 'got 1:00' );
+		assert.ok(convert.SecondsInColonTime(61) === '1:01', 'got 1:01' );
+		assert.ok(convert.SecondsInColonTime(130) === '2:10', 'got 2:10' );
+		assert.ok(convert.SecondsInColonTime(7442) === '2:04:02', 'got 2:04:02' );
 	});
 
 	var cpu = document.CPU;
 
-
-	waitNoLock();
-	nowLock();
-	QUnit.asyncTest( "document.CPU.jumpIdAt existing at start", function( assert ) {
-		expect( 2 );
+	//waitNoLock();
+	//nowLock();
+	QUnit.test( "document.CPU.jumpIdAt existing at start", function( assert ) {
+		assert.expect( 2 );
+		let done = assert.async();
 		cpu.jumpIdAt('track', 0, function() {
 			assert.ok(audiotag.currentTime === 0, 'is at start' );
 			assert.ok(!audiotag.paused, 'not paused afterwards' );
-			QUnit.start();
 			stopPlayer();
+			done();
 		});
 	});
 
-	waitNoLock();
-	nowLock();
-	QUnit.asyncTest( "document.CPU.jumpIdAt existing at 600 secs", function( assert ) {
-		expect( 1 );
+	//waitNoLock();
+	//nowLock();
+	QUnit.test( "document.CPU.jumpIdAt existing at 600 secs", function( assert ) {
+		assert.expect( 1 );
+		let done = assert.async();
 		cpu.jumpIdAt('track', 600, function() {
 			assert.ok(audiotag.currentTime === 600, 'is at 10mn' );
-			QUnit.start();
-			stopPlayer();
+			//stopPlayer();
+			done();
 		});
 	});
 
-	waitNoLock();
-	nowLock();
-	QUnit.asyncTest( "document.CPU.jumpIdAt unnamed at 300 secs", function( assert ) {
-		expect( 1 );
+	//waitNoLock();
+	//nowLock();
+	QUnit.test( "document.CPU.jumpIdAt unnamed at 300 secs", function( assert ) {
+		assert.expect( 1 );
+		let done = assert.async();
 		cpu.jumpIdAt('', 300, function() {
 			assert.ok(audiotag.currentTime === 300, 'is at 5mn' );
-			QUnit.start();
-			stopPlayer();
+			//stopPlayer();
+			done();
 		});
 	});
-
-
 
 	function hashOrder_test(expected_string, hash , expected_time)
 	{
-		waitNoLock();
-		nowLock();
-		QUnit.asyncTest( `document.CPU.trigger.hashOrder ${expected_string}`, function( assert ) {
+		//waitNoLock();
+		//nowLock();
+		QUnit.test( `document.CPU.trigger.hashOrder ${expected_string}`, function( assert ) {
 			nowLock();
-			expect( 1 );
+			assert.expect( 1 );
+			let done = assert.async();
 			cpu.trigger.hashOrder(hash, function() {
 				assert.ok(audiotag.currentTime === expected_time, expected_string);
-				QUnit.start();
-				stopPlayer();
+				//stopPlayer();
+				done();
 			});
 		});
 	}
@@ -129,11 +134,11 @@ window.addEventListener('WebComponentsReady', function() {
 	hashOrder_test('track is at 02:04:02', 'track&t=01:04:02', 3842);
 	hashOrder_test('unnamed track is at 1:02', '&t=1:02', 62);
 
-
-	waitNoLock();
-	nowLock();
-	QUnit.asyncTest( "no cacophony feature : mute other player when another one starts to play", function( assert ) {
-		expect(2);
+	//waitNoLock();
+	//nowLock();
+	QUnit.test( "no cacophony feature : mute other player when another one starts to play", function( assert ) {
+		let done = assert.async();
+		assert.expect(2);
 		audiotag.play();
 		// Dynamic add a second audio player
 		playground.innerHTML = `
@@ -144,27 +149,28 @@ window.addEventListener('WebComponentsReady', function() {
 			</audio>
 		</cpu-audio>`;
 		let secondary_audiotag = document.getElementById('secondary');
+		secondary_audiotag.volume = 0;
 		let check_only_one_play_this;
 
 		function check_only_one_play() {
 			assert.ok(! secondary_audiotag.paused, 'Second player should continue to play');
 			assert.ok(audiotag.paused, 'First player should have been paused');
-			QUnit.start();
-			stopPlayer();
+			// stopPlayer();
+			done();
 		}
 		check_only_one_play_this = check_only_one_play.bind(this);
 		setTimeout(check_only_one_play_this, 100);
 		secondary_audiotag.play();
 	});
 
-
-	waitNoLock();
-	nowLock();
+	// waitNoLock();
+	// nowLock();
 	// Try trigger.hashOrder({ at_start : true }); with hash link
-	QUnit.asyncTest( "Startup page with a hash link and without localStorage still played", function(assert) {
-		waitNoLock();
-		nowLock();
-		expect(3);
+	QUnit.test( "Startup page with a hash link and without localStorage still played", function(assert) {
+		let done = assert.async();
+		//waitNoLock();
+		//nowLock();
+		assert.expect(3);
 		playground.innerHTML = `
 			<cpu-audio>
 				<audio id="secondary" controls="controls">
@@ -173,30 +179,32 @@ window.addEventListener('WebComponentsReady', function() {
 				</audio>
 			</cpu-audio>`;
 		let secondary_audiotag = document.getElementById('secondary');
+		secondary_audiotag.volume = 0;
 		localStorage.clear();
 		function check_onstart() {
 			assert.ok(! secondary_audiotag.paused, 'Second player should have started');
 			assert.ok(secondary_audiotag.currentTime = 10, 'Second player should be at 10s');
 			assert.ok(audiotag.paused, 'First player should be paused');
-			QUnit.start();
-			stopPlayer();
+			//stopPlayer();
 			window.addEventListener('hashchange', cpu.trigger.hashOrder, false);
+			done();
 		}
 		// désactiver provisoirement le hashchange event
+		setTimeout(check_onstart, 200);
 		window.removeEventListener('hashchange', cpu.trigger.hashOrder);
 		window.location = '#secondary&t=10';
 		cpu.trigger.hashOrder({ at_start : true });
-		setTimeout(check_onstart, 100);
 		
 	});
 
-	waitNoLock();
-	nowLock();
+	//waitNoLock();
+	//nowLock();
 	// Try trigger.hashOrder({ at_start : true }); with hash link
-	QUnit.asyncTest( "Startup page without hash link and with a localStorage recalling", function(assert) {
-		waitNoLock();
-		nowLock();
-		expect(3);
+	QUnit.test( "Startup page without hash link and with a localStorage recalling", function(assert) {
+		let done = assert.async();
+		//waitNoLock();
+		//nowLock();
+		assert.expect(3);
 		playground.innerHTML = `
 			<cpu-audio>
 				<audio id="secondary" controls="controls">
@@ -205,30 +213,32 @@ window.addEventListener('WebComponentsReady', function() {
 				</audio>
 			</cpu-audio>`;
 		let secondary_audiotag = document.getElementById('secondary');
+		secondary_audiotag.volume = 0;
 		localStorage.setItem(secondary_audiotag.currentSrc, String(30));
 		localStorage.clear();
 		function check_onstart() {
 			assert.ok(! secondary_audiotag.paused, 'Second player should have started');
 			assert.ok(secondary_audiotag.currentTime = 30, 'Second player should be at 30s');
 			assert.ok(audiotag.paused, 'First player should be paused');
-			QUnit.start();
-			stopPlayer();
+			//stopPlayer();
 			window.addEventListener('hashchange', cpu.trigger.hashOrder, false);
+			done();
 		}
 		// désactiver provisoirement le hashchange event
 		window.removeEventListener('hashchange', cpu.trigger.hashOrder);
+		setTimeout(check_onstart, 200);
 		window.location = '#';
 		cpu.trigger.hashOrder({ at_start : true });
-		setTimeout(check_onstart, 100);
 	});
 
-	waitNoLock();
-	nowLock();
+	// waitNoLock();
+	// nowLock();
 	// Try trigger.hashOrder({ at_start : true }); with hash link
-	QUnit.asyncTest( "Startup page with a hash link and with a localStorage recalling", function(assert) {
-		waitNoLock();
-		nowLock();
-		expect(3);
+	QUnit.test( "Startup page with a hash link and with a localStorage recalling", function(assert) {
+		let done = assert.async();
+		//waitNoLock();
+		//nowLock();
+		assert.expect(3);
 		playground.innerHTML = `
 			<cpu-audio>
 				<audio id="secondary" controls="controls">
@@ -237,21 +247,22 @@ window.addEventListener('WebComponentsReady', function() {
 				</audio>
 			</cpu-audio>`;
 		let secondary_audiotag = document.getElementById('secondary');
+		secondary_audiotag.volume = 0;
 		localStorage.setItem(secondary_audiotag.currentSrc, String(30));
 		localStorage.clear();
 		function check_onstart() {
 			assert.ok(! secondary_audiotag.paused, 'Second player should have started');
 			assert.ok(secondary_audiotag.currentTime = 30, 'Second player should be at 30s');
 			assert.ok(audiotag.paused, 'First player should be paused');
-			QUnit.start();
-			stopPlayer();
+			//stopPlayer();
 			window.addEventListener('hashchange', cpu.trigger.hashOrder, false);
+			done();
 		}
 		// désactiver provisoirement le hashchange event
 		window.removeEventListener('hashchange', cpu.trigger.hashOrder);
+		setTimeout(check_onstart, 200);
 		window.location = '#track&t=10';
 		cpu.trigger.hashOrder({ at_start : true });
-		setTimeout(check_onstart, 100);
 		
 	});
 
