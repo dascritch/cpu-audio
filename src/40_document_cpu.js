90002edc5a3b58d8ca78a9de8f8331c144357173
dascritch <xaviermd@gmail.com>
2018-10-16 13:34:12 +0200

    Remove unuseful events

--- src/40_document_cpu.js
+++ src/40_document_cpu.js
@@ -78,9 +78,9 @@ document.CPU = document.CPU ? document.CPU : {
         // see https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events for list of events
         [
             'ready', 'load', 'loadeddata', 'canplay', 'abort', 
-            'error', 'stalled', 'suspend', 'emptied',
+            'error', /*'stalled',*/ 'suspend', 'emptied',
             'play', 'playing', 'pause', 'ended',
-            'durationchange',  'loadedmetadata', 'progress', 'timeupdate', 'waiting'
+            'durationchange',  'loadedmetadata', /*'progress',*/ 'timeupdate', 'waiting'
         ].forEach( function(on){ 
             audiotag.addEventListener(on, trigger.update); 
         });
