5ffc47a540447bccc214a4a3f9825bb59c59f2be
Xavier Mouton-Dubosc <xavier@dascritch.com>
2021-02-22 08:25:16 +0100

    Add a browser test for ECMA2019

--- tests/tests-browser.js
+++ tests/tests-browser.js
@@ -19,6 +19,11 @@ QUnit.test( "Checking browser abilities for WebComponents", function( assert ) {
 	assert.ok( window.customElements !== undefined, "window.customElements" );
 });
 
+QUnit.test( "Checking browser can use ECMA 2019", function( assert ) {
+	assert.ok( Object.fromEntries !== undefined, "Object.fromEntries" );
+});
+
+
 QUnit.test( "<audio> tag API, fallback suite for seekElementAt()", function( assert ) {
 	assert.ok( audiotag.fastSeek !== undefined, "seekElementAt can use audiotag.fastSeek" );
 	assert.ok( audiotag.currentTime !== undefined, "seekElementAt can use audiotag.currentTime" );
