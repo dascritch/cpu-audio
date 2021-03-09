513ae7216695aa0da798057dbf6fedb043da56f6
Xavier Mouton-Dubosc <xavier@dascritch.com>
2021-03-09 18:58:37 +0100

    Resolve differences between Chrome desktop and Chrome puppeteered
    Do more tests on real mobile Chrome on real websites #113

--- tests/tests-minimal.js
+++ tests/tests-minimal.js
@@ -14,7 +14,10 @@ if (!document.hasFocus()) {
 }
 
 window.addEventListener('load', function() {
-	document.querySelector('#get_focus').click();
+	if (navigator.userAgent === 'puppeteer') {
+		// Tests fails on Chrome browszer is this button is not humanly clicked, except in puppeteer /o\
+		document.querySelector('#get_focus').click();
+	}
 });
 
 document.querySelector('#get_focus').addEventListener('click', function() {
