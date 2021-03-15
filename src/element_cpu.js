ab2bc3b7f3a8e315876ba5e92685ff3d82fd98e6
Xavier Mouton-Dubosc <xavier@dascritch.com>
2021-03-15 07:13:46 +0100

    separate finger_manager from element_cpu

--- src/element_cpu.js
+++ src/element_cpu.js
@@ -1,8 +1,9 @@
-import {dynamically_allocated_id_prefix, passive_ev, once_passive_ev, on_debug, CpuControllerTagName, absolutize_url, escape_html, querySelector_apply, is_audiotag_streamed, error} from './utils.js';
+import {dynamically_allocated_id_prefix, passive_ev, once_passive_ev, CpuControllerTagName, absolutize_url, escape_html, querySelector_apply, is_audiotag_streamed, error} from './utils.js';
 import {__, prefered_language} from './i18n.js';
 import {convert} from './convert.js';
 import {trigger} from './trigger.js';
 import {translate_vtt} from './translate_vtt.js';
+import {finger_manager} from './finger_manager.js';
 
 
 // Acceptables attributes values for hide="" parameter on webcomponent
@@ -16,98 +17,6 @@ const valid_id = /^[a-zA-Z0-9\-_]+$/;
 
 // Regex for extracting plane and point names from an id
 const plane_point_names_from_id = /^([a-zA-Z0-9\-_]+_«)([a-zA-Z0-9\-_]+)((»_.*_«)([a-zA-Z0-9\-_]+))?(»)$/;
-
-// Handheld navigation button process
-// @private
-let finger_manager = {
-	// Repeated event allocation
-	is_on : null,
-
-	/*
-	 * @summary Start handheld navigation button press
-	 * @private
-	 *
-	 * @param      {Object}  event   The event
-	 */
-	press : function(event) {
-		let target = event.target.id ? event.target : event.target.closest('button');
-		let acceptable_actions = ['fastreward', 'reward', 'foward', 'fastfoward'];
-		if ( (!target.id) || (acceptable_actions.indexOf(target.id) === -1)) {
-			// we have been misleaded
-			return;
-		}
-		// execute the associated function
-		trigger[target.id](event);
-		if (finger_manager.is_on !== null) {
-			window.clearTimeout(finger_manager.is_on);
-		}
-
-		let mini_event = {
-			target : target,
-			preventDefault : on_debug
-		};
-		finger_manager.is_on = window.setTimeout(finger_manager.repeat, document.CPU.repeat_delay, mini_event);
-		event.preventDefault();
-	},
-	/*
-	 * @summary Repeat during pressing handheld navigation button
-	 * @private
-	 *
-	 * @param      {Object}  event   The event
-	 */
-	repeat : function(event) {
-		// 
-		trigger[event.target.id](event);
-		// next call : repetition are closest
-		finger_manager.is_on = window.setTimeout(finger_manager.repeat, document.CPU.repeat_factor, event);
-	},
-	/*
-	 * @summary Release handheld navigation button
-	 * @private
-	 *
-	 * @param      {Object}  event   The event
-	 */
-	release : function(event) {
-		window.clearTimeout(finger_manager.is_on);
-		finger_manager.is_on = null;
-		event.preventDefault();
-	},
-
-	/**
-	 * @summary Interprets long play on timeline to reveal alternative fine position panel
-	 */
-	touching : null,
-	/**
-	 * @summary Press started on the timeline
-	 *
-	 * @param      {Object}  event   The event
-	 */
-	touchstart : function(event) {
-		let container = document.CPU.find_container(event.target);
-		finger_manager.touching = setTimeout(container.show_handheld_nav.bind(container), document.CPU.alternate_delay);
-	},
-	/**
-	 * @summary Press stoped on the timeline
-	 *
-	 * @param      {Object}  event   The event
-	 */
-	touchcancel : function(/* event */) {
-		clearTimeout(finger_manager.touching);
-	},
-
-	/**
-	 * @summary Menu launch. Right Mouse Button on desktop, but may be a pointer long-press, we try to avoid this last one
-	 *
-	 * @param      {Object}  event   The event
-	 */
-	rmb : function(event) {
-		let container = document.CPU.find_container(event.target);
-		if (!finger_manager.touching) {
-			container.show_handheld_nav();
-		}
-		event.preventDefault();
-	}
-};
 
 export class CPU_element_api {
 	/**
---
+++ src/finger_manager.js
@@ -0,0 +1,94 @@
+import {on_debug} from './utils.js';
+import {trigger} from './trigger.js';
+
+// Handheld navigation button process
+// @private
+export let finger_manager = {
+	// Repeated event allocation
+	is_on : null,
+
+	/*
+	 * @summary Start handheld navigation button press
+	 * @private
+	 *
+	 * @param      {Object}  event   The event
+	 */
+	press : function(event) {
+		let target = event.target.id ? event.target : event.target.closest('button');
+		let acceptable_actions = ['fastreward', 'reward', 'foward', 'fastfoward'];
+		if ( (!target.id) || (acceptable_actions.indexOf(target.id) === -1)) {
+			// we have been misleaded
+			return;
+		}
+		// execute the associated function
+		trigger[target.id](event);
+		if (finger_manager.is_on !== null) {
+			window.clearTimeout(finger_manager.is_on);
+		}
+
+		let mini_event = {
+			target : target,
+			preventDefault : on_debug
+		};
+		finger_manager.is_on = window.setTimeout(finger_manager.repeat, document.CPU.repeat_delay, mini_event);
+		event.preventDefault();
+	},
+	/*
+	 * @summary Repeat during pressing handheld navigation button
+	 * @private
+	 *
+	 * @param      {Object}  event   The event
+	 */
+	repeat : function(event) {
+		// 
+		trigger[event.target.id](event);
+		// next call : repetition are closest
+		finger_manager.is_on = window.setTimeout(finger_manager.repeat, document.CPU.repeat_factor, event);
+	},
+	/*
+	 * @summary Release handheld navigation button
+	 * @private
+	 *
+	 * @param      {Object}  event   The event
+	 */
+	release : function(event) {
+		window.clearTimeout(finger_manager.is_on);
+		finger_manager.is_on = null;
+		event.preventDefault();
+	},
+
+	/**
+	 * @summary Interprets long play on timeline to reveal alternative fine position panel
+	 */
+	touching : null,
+	/**
+	 * @summary Press started on the timeline
+	 *
+	 * @param      {Object}  event   The event
+	 */
+	touchstart : function(event) {
+		let container = document.CPU.find_container(event.target);
+		finger_manager.touching = setTimeout(container.show_handheld_nav.bind(container), document.CPU.alternate_delay);
+	},
+	/**
+	 * @summary Press stoped on the timeline
+	 *
+	 * @param      {Object}  event   The event
+	 */
+	touchcancel : function(/* event */) {
+		clearTimeout(finger_manager.touching);
+	},
+
+	/**
+	 * @summary Menu launch. Right Mouse Button on desktop, but may be a pointer long-press, we try to avoid this last one
+	 *
+	 * @param      {Object}  event   The event
+	 */
+	rmb : function(event) {
+		let container = document.CPU.find_container(event.target);
+		if (!finger_manager.touching) {
+			container.show_handheld_nav();
+		}
+		event.preventDefault();
+	}
+};
