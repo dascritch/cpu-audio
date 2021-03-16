6b87003faf8e3189705dcd8b9c7c625dfd7c4931
Xavier Mouton-Dubosc <xavier@dascritch.com>
2021-03-16 21:31:22 +0100

    move default_dataset out of document.CPU core code

--- src/document_cpu.js
+++ src/document_cpu.js
@@ -3,6 +3,46 @@ import {CpuAudioTagName, CpuControllerTagName, is_audiotag_streamed, on_debug, o
 import {convert} from './convert.js';
 import {trigger} from './trigger.js';
 
+const default_dataset = {
+	get title() {
+		for (let domain of ['og', 'twitter']) {
+			let header_element = document.querySelector(`meta[property="${domain}:title"]`);
+			if (header_element !== null) {
+				return header_element.content;
+			}
+		}
+		let title = document.title;
+		return title === '' ? null : title;		
+	},
+	get poster() {
+		for (let attr of ['property="og:image"', 'name="twitter:image:src"']) {
+			let header_element = document.querySelector(`meta[${attr}]`);
+			if (header_element !== null) {
+				return header_element.content;
+			}
+		}
+		return null;
+	},
+	get canonical() {
+		let header_element = document.querySelector('link[rel="canonical"]');
+		if (header_element !== null) {
+			return header_element.href;
+		}
+		return location.href.split('#')[0];
+	},
+	get twitter() {
+		let header_element = document.querySelector('meta[name="twitter:creator"]');
+		if ((header_element !== null) && (header_element.content.length>1)) {
+			return header_element.content;
+		}
+		return null;
+	},
+	playlist : null,
+	waveform : null,
+	duration : null,
+	download : null
+};
+
 export let document_CPU = {
 	// global object for global API
 
@@ -69,50 +109,12 @@ export let document_CPU = {
 
 	// public, Exposing internals needed for tests
 	// @public
-	'convert' : convert,
+	convert,
 	// @public
-	'trigger' : trigger,
+	trigger,
 
 	// @package, not enough mature
-	default_dataset : {
-		get title() {
-			for (let domain of ['og', 'twitter']) {
-				let header_element = document.querySelector(`meta[property="${domain}:title"]`);
-				if (header_element !== null) {
-					return header_element.content;
-				}
-			}
-			let title = document.title;
-			return title === '' ? null : title;		
-		},
-		get poster() {
-			for (let attr of ['property="og:image"', 'name="twitter:image:src"']) {
-				let header_element = document.querySelector(`meta[${attr}]`);
-				if (header_element !== null) {
-					return header_element.content;
-				}
-			}
-			return null;
-		},
-		get canonical() {
-			let header_element = document.querySelector('link[rel="canonical"]');
-			if (header_element !== null) {
-				return header_element.href;
-			}
-			return location.href.split('#')[0];
-		},
-		get twitter() {
-			let header_element = document.querySelector('meta[name="twitter:creator"]');
-			if ((header_element !== null) && (header_element.content.length>1)) {
-				return header_element.content;
-			}
-			return null;
-		},
-		playlist : null,
-		waveform : null,
-		duration : null,
-		download : null
-	},
+	default_dataset,
 
 	/**
 	 * @public
