42ee8ab7a33a1ae75b93aff1c0119234f3b32e94
Xavier Mouton-Dubosc <xavier@dascritch.com>
2021-03-18 20:51:40 +0100

    Convertiing finger_manager
    Refactoring with ECMA classes #115

--- src/finger_manager.js
+++ src/finger_manager.js
@@ -3,17 +3,19 @@ import {trigger} from './trigger.js';
 
 // Repeated event allocation
 let is_on = null;
+// touch is occuring
+let touching = null;
 
 // Handheld navigation button process
 // @private
-export let finger_manager = {
+export class finger_manager {
 	/*
 	 * @summary Start handheld navigation button press
 	 * @private
 	 *
 	 * @param      {Object}  event   The event
 	 */
-	press : function(event) {
+	press(event) {
 		let target = event.target.id ? event.target : event.target.closest('button');
 		let acceptable_actions = ['fastreward', 'reward', 'foward', 'fastfoward'];
 		if ( (!target.id) || (!acceptable_actions.includes(target.id))) {
@@ -32,63 +34,63 @@ export let finger_manager = {
 		};
 		is_on = window.setTimeout(finger_manager.repeat, document.CPU.repeat_delay, mini_event);
 		event.preventDefault();
-	},
+	}
 	/*
 	 * @summary Repeat during pressing handheld navigation button
 	 * @private
 	 *
 	 * @param      {Object}  event   The event
 	 */
-	repeat : function(event) {
+	repeat(event) {
 		//
 		trigger[event.target.id](event);
 		// next call : repetition are closest
 		is_on = window.setTimeout(finger_manager.repeat, document.CPU.repeat_factor, event);
-	},
+	}
 	/*
 	 * @summary Release handheld navigation button
 	 * @private
 	 *
 	 * @param      {Object}  event   The event
 	 */
-	release : function(event) {
+	release(event) {
 		window.clearTimeout(is_on);
 		is_on = null;
 		event.preventDefault();
-	},
+	}
 
 	/**
 	 * @summary Interprets long play on timeline to reveal alternative fine position panel
 	 */
-	touching : null,
+	
 	/**
 	 * @summary Press started on the timeline
 	 *
 	 * @param      {Object}  event   The event
 	 */
-	touchstart : function({target}) {
+	touchstart({target}) {
 		let container = document.CPU.find_container(target);
-		finger_manager.touching = setTimeout(container.show_handheld_nav.bind(container), document.CPU.alternate_delay);
-	},
+		touching = setTimeout(container.show_handheld_nav.bind(container), document.CPU.alternate_delay);
+	}
 	/**
 	 * @summary Press stoped on the timeline
 	 *
 	 * @param      {Object}  event   The event
 	 */
-	touchcancel : function(/* event */) {
-		clearTimeout(finger_manager.touching);
-	},
+	touchcancel() {
+		clearTimeout(touching);
+	}
 
 	/**
 	 * @summary Menu launch. Right Mouse Button on desktop, but may be a pointer long-press, we try to avoid this last one
 	 *
 	 * @param      {Object}  event   The event
 	 */
-	rmb : function(event) {
+	rmb(event) {
 		let container = document.CPU.find_container(event.target);
-		if (!finger_manager.touching) {
+		if (!touching) {
 			container.show_handheld_nav();
 		}
 		event.preventDefault();
 	}
-};
+}
