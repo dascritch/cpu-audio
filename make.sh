53e58b0b0367dd42d7d93b1734b16f95823eadd8
Xavier Mouton-Dubosc <xavier@dascritch.com>
2021-04-12 07:27:54 +0200

    Add in automation
    Create persistent cpu-controller #87

--- make.sh
+++ make.sh
@@ -179,7 +179,7 @@ function _build_component_js_webpack() {
 
 function _tests() {
 	# watchout to NOT inclue "browser", which is only a browsder comptability test, and Chrome fails it
-	for chapter in minimal api interface; do
+	for chapter in minimal api interface controller; do
     	npx node-qunit-puppeteer "tests/tests-${chapter}.html" 150000 "--allow-file-access-from-files --no-sandbox --autoplay-policy=no-user-gesture-required --user-agent=puppeteer"
 	done
 }
