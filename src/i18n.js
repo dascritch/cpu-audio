b6f84ca1b2e18b5402c4f5c8d107a783c9fb767f
Xavier Mouton-Dubosc <xavier.mouton-dubosc@humansconnexion.com>
2022-09-27 14:26:31 +0200

    A very old piece of code, needed a serious refresh

--- src/i18n.js
+++ src/i18n.js
@@ -53,29 +53,34 @@ const sources_i18n = {
 	},
 };
 
-// First, we'll try to guess the hosting page language
-export let prefered_language = document.querySelector('html').lang;
-
-if ((!prefered_language.length) || (!(prefered_language.toLowerCase() in sources_i18n))) {
-
-	// Inexisting ?
-	prefered_language = 'en';
-	// We will use english in last resort
+/**
+ * @summary Guess usable language (returned in ISO 2 letters code) from browser preferences and host page mark-up
+ * @private
+ * 
+ */
+function guess_preferable_language() {
+	// First, we'll try to guess the hosting page language
+	const out = document.querySelector('html').lang;
+	if ((out.length) && (out.toLowerCase() in sources_i18n)) {
+		return out;
+	}
 
 	// trying to find the browser preferences
 	const languages = window.navigator.languages ?? [(navigator.language || navigator.browserLanguage)];
-	let added = false;
 	for (const line of languages) {
 		if (line.split) {
 			// we will only look (yes, this is bad) at the first level xx of any locale xx-YY code
 			const [code] = line.split('-');
-			if ( (!added) && (code in sources_i18n)) {
+			if (code in sources_i18n) {
 				// we still don't have a locale selected and this one is in our register, we can use it
-				prefered_language = code;
-				added = true;
+				return code;
 			}
 		}
 	}
+
+	// Inexisting ? We will use english in last resort	
+	return 'en';
 }
 
+export let prefered_language = guess_preferable_language();
 export const __ = sources_i18n[prefered_language];
