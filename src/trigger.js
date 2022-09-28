62023cc56cbfc734d7c40c67f17562a7074e1b49
Xavier Mouton-Dubosc <xavier.mouton-dubosc@humansconnexion.com>
2022-09-28 16:02:06 +0200

    Avoid exception on querySelector('#')
    Parametrable : scroll to the audio element #60

--- src/trigger.js
+++ src/trigger.js
@@ -163,9 +163,11 @@ export const trigger = {
 		}
 
 		// scroll to the audio element. Should be reworked, or parametrable , see issue #60
-		if ((document.CPU.scrollTo) && (hash)) {
+		if (document.CPU.scrollTo) {
 			// if the hash was not found, rely on the actual player
-			(document.querySelector(`#${hash}`) ?? document.CPU.currentAudiotagPlaying).closest('cpu-audio, cpu-controller').scrollIntoView();
+			( 
+				(hash?.length > 0) ? document.querySelector(`#${hash}`) : document.CPU.currentAudiotagPlaying
+			)?.closest('cpu-audio, cpu-controller').scrollIntoView();
 		}
 
 		await document.CPU.jumpIdAt( hash??'', timecode_start, callback_fx);
