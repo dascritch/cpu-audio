cd01827bef4d7217ad70b8922b5192a7ac4e355a
Xavier Mouton-Dubosc <xavier.mouton-dubosc@humansconnexion.com>
2022-09-28 17:04:22 +0200

    re-order and categorize global settable parameters

--- src/default_document_cpu_parameters.js
+++ src/default_document_cpu_parameters.js
@@ -1,24 +1,35 @@
 export const DefaultParametersDocumentCPU = {
 	// those values may be later modified via loading parameters
 
-	// @public
-	// @type boolean
-	scrollTo : false,
+	// player basic
 
-	// @public
-	// @type boolean
-	autoplay : false,
-
-	// @public
-	// @type number
-	keymove : 5,
 	// @public
 	// @type boolean
 	playStopOthers : true,
 	// @public
+	// @type boolean
+	scrollTo : false,
+	// @public
+	// @type boolean
+	autoplay : false,
+
+	// playlist interactions
+
+	// @public
+	// @type boolean
+	advanceInPlaylist : true,
+
+	// Keyboard navigation
+
+	// @public
+	// @type number
+	keymove : 5,
+
+	// Fine navigation
+
+	// @public
 	// @type number
 	alternateDelay : 1000,
-
 	// @public
 	// @type number
 	fastFactor : 4,
@@ -28,9 +39,5 @@ export const DefaultParametersDocumentCPU = {
 	// @public
 	// @type number
 	repeatFactor : 100,
-
-	// @public
-	// @type boolean
-	advanceInPlaylist : true,
 
 };
